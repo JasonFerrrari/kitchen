75e3645c31ab049546bef6b267ce82d415650112
JasonFerrrari <jason.ferrari@uconn.edu>
2021-02-10 18:40:03 -0500

    first commmit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010166",
+      "name": "R0010166",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.6877185842525524,
+        "pitch": -0.02286347702431435,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5212222501956525,
+          "pitch": 0.14591020568395763,
+          "rotation": 0,
+          "target": "1-r0010164"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0010164",
+      "name": "R0010164",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.90351831590288,
+        "pitch": 0.27426692790313645,
+        "fov": 2.0943951023931953
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10707196521792284,
+          "pitch": 0.19978283906026384,
+          "rotation": 0,
+          "target": "0-r0010166"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
